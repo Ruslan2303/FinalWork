d281f70666dedf91f266bc4c90609ec53814fbc6
Kirill <fedenko.kirill@gmail.com>
2025-05-11 21:03:09 +0300

    add comment, positivity check, and results output to the S variable

--- lab4.cpp
+++ lab4.cpp
@@ -1,10 +1,15 @@
-﻿
+
 #include <iostream>
 
 int main()
 {
-    int S = 100;
+    int S = 100;//the last element of a natural progression, up to which the sum of squares is calculated
     int sum = 0;
+
+    if (S > 100){//checks if a variable S is positive
+        printf_s("Variable 'S' is positive");
+    }
+
     for (int i = 0; i < S; i++)
     {
         sum += i * i;
