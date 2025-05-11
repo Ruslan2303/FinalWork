156fa49cabf2f16b8bb0b06474a0e56821b8fec7
Kirill <fedenko.kirill@gmail.com>
2025-05-11 22:59:45 +0300

    edit lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -6,7 +6,7 @@ int main()
     int S = 100;//the last element of a natural progression, up to which the sum of squares is calculated
     int sum = 0;
 
-    if (S > 100){//checks if a variable S is positive
+    if (S > 100){//checks if the variable S is positive
         printf_s("Variable 'S' is positive");
     }
 
