911b8d1a09e7e89ec9abc1b529385d02a338b4d5
Kirill <fedenko.kirill@gmail.com>
2025-05-13 12:43:33 +0300

    add rand array to lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -5,6 +5,7 @@ int main()
 {
     int S = 100;//the last element of a natural progression, up to which the sum of squares is calculated
     int sum = 0;
+    int array[5];
 
     if (S > 100){//checks if a variable S is positive
         printf_s("Variable 'S' is positive");
@@ -15,4 +16,11 @@ int main()
         sum += i * i;
     }
     printf("%d", sum);
+
+    srand(time(nullptr));
+    printf("\n");
+    for (int i = 0; i < 5; i++) {
+        array[i] = rand() % 21 - 10;
+        printf("%d ", array[i]);
+    }
 }
